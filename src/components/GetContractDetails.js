62dacdba550bec85cb903d2ab6e15bd81b4013e2
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-09-20 11:07:36 +0300

    yesterday bug is fixed. I can now fetch multiple return values from solidity with ethers

--- src/components/GetContractDetails.js
+++ src/components/GetContractDetails.js
@@ -8,18 +8,22 @@ function GetContractDetails() {
   let[stockholmOwner, setStockholmOwner] = useState("");
   let contract;
   let signer;
+  const CONTRACT_ABI = ABI;
 
   const connectContract = async () => {
     const Address = "0xb5dd0dFEEC4C94eA3f9573018aB009DEb497Ba28";
+    const ABI = CONTRACT_ABI;
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     signer = provider.getSigner();
-    contract = new ethers.Contract(ABI, Address, signer);
+    contract = new ethers.Contract(Address, ABI, signer);
   }
 
   const getData = async () => {
     connectContract();
     const txResponse = await contract.getDetails();
-    console.log(txResponse)
+    const {0: owner, 1: contractAddress} = txResponse;
+    setStockholmAddress(contractAddress);
+    setStockholmOwner(owner);
   }
 
   return (
