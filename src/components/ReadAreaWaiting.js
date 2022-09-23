0e187b8dbf5eac2496d037697c9284d2bcc3c220
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-09-23 20:53:53 +0300

    reading proposals array component created but not finished yet. currently it prints successfull to the console

--- src/components/ReadAreaWaiting.js
+++ src/components/ReadAreaWaiting.js
@@ -7,17 +7,34 @@ import {ABI} from "./ContractABI";
 
 function ReadAreaWaiting() {
     const navigate = useNavigate();
-    let[waitingProposals, setWaitingProposals] = useState("");
+
 
     let contract;
     let signer;
     const CONTRACT_ABI = ABI;
     const ADDRESS = CONTRACT_ADDRESS;
 
+    const connectContract = async () => {
+        const Address = ADDRESS;
+        const ABI = CONTRACT_ABI;
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        signer = provider.getSigner();
+        contract = new ethers.Contract(Address, ABI, signer);
+    }
+
+    const waitingPro = async () => {
+        connectContract();
+        const txResponse = await contract.getAllPro();
+        console.log(txResponse);
+    }
+    
+
+
+
     return (
         <div>
             <h3>Waiting Proposals:</h3>
-            {waitingProposals}
+            <button onClick={waitingPro} className="button-56" >Some data</button>
             <br />
             <button className="button-56" onClick={ () => navigate("/")}>Homepage</button>
         </div>
